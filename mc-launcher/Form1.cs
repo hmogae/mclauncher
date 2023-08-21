d6eb7146f863d85d7614b10f3ce8201dc084f02c
애플망고 <107789784+hmogae@users.noreply.github.com>
2023-08-21 16:38:01 +0900

    Add files via upload

---
+++ mc-launcher/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace mc_launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("[마인크래프트]를 실행할까요?", "알림", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (result == DialogResult.Yes)
+            {
+                Process.Start("C:\\Program Files (x86)\\Minecraft Launcher\\MinecraftLauncher.exe"); //경로
+                Application.Exit();
+            }
+
+            else if (result == DialogResult.No)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("알수없는 예외 오류가 발생하였습니다.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+    }
+}
